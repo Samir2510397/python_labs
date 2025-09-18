76c1108045418b3ef42eb15d6435b0c4f44a6a50
Samir2510397 <Samirabdullin2007@gmail.com>
2025-09-18 19:34:58 +0300

    0_0

--- src/lab01/exe07.py
+++ src/lab01/exe07.py
@@ -1,9 +1,9 @@
-ishod = input()
-print("in: " + ishod)
+a = input()
+print("in: " + a)
 c = []
-for i in range(len(ishod)):
-    if ishod[i].isupper():
+for i in range(len(a)):
+    if a[i].isupper():
         cnt = i
         break
-c.append(''.join([ishod[i] for i in range(cnt, len(ishod), 3)]))
+c.append(''.join([a[i] for i in range(cnt, len(a), 3)]))
 print("out:", *c)
