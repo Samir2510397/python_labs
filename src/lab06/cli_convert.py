44ec9701571958173264e718a503e9b9ab268a7c
Samir2510397 <Samirabdullin2007@gmail.com>
2025-11-13 20:22:09 +0300

    update

--- src/lab06/cli_convert.py
+++ src/lab06/cli_convert.py
@@ -2,33 +2,32 @@ import argparse
 from src.lib import json_to_csv, csv_to_json, csv_to_xlsx
 
 def main():
-    parser = argparse.ArgumentParser(description="Конвертеры данных")
-    sub = parser.add_subparsers(dest="cmd")
+    parser = argparse.ArgumentParser(description="Конвертеры данных")#Создаем основной парсер с описанием "Конвертеры данных"
+    sub = parser.add_subparsers(dest="cmd")#Создаем группу подпарсеров для разных команд ,а выбор команды будет сохранен в атрибут cmd
 
-    json_to_csv_p = sub.add_parser("json2csv")
-    json_to_csv_p.add_argument("--in", dest="input", required=True, help="Входной JSON файл")
-    json_to_csv_p.add_argument("--out", dest="output", required=True, help="Выходной CSV файл")
 
-    csv_to_json_p = sub.add_parser("csv2json")
-    csv_to_json_p.add_argument("--in", dest="input", required=True, help="Входной CSV файл")
-    csv_to_json_p.add_argument("--out", dest="output", required=True, help="Выходной JSON файл")
+    json_to_csv_p = sub.add_parser("json2csv")#Создаем подпарсер для команды json2csv
+    json_to_csv_p.add_argument("--input", dest="input", required=True, help="Входной JSON файл")
+    json_to_csv_p.add_argument("--output", dest="output", required=True, help="Выходной CSV файл")
 
-    csv_to_xlsx_p = sub.add_parser("csv2xlsx")
-    csv_to_xlsx_p.add_argument("--in", dest="input", required=True, help="Входной CSV файл")
-    csv_to_xlsx_p.add_argument("--out", dest="output", required=True, help="Выходной XLSX файл")
+    csv_to_json_p = sub.add_parser("csv2json")#Создаем подпарсер для команды csv2json
+    csv_to_json_p.add_argument("--input", dest="input", required=True, help="Входной CSV файл")
+    csv_to_json_p.add_argument("--output", dest="output", required=True, help="Выходной JSON файл")
 
-    args = parser.parse_args()
+    csv_to_xlsx_p = sub.add_parser("csv2xlsx")#Создаем подпарсер для команды csv2xlsx
+    csv_to_xlsx_p.add_argument("--input", dest="input", required=True, help="Входной CSV файл")
+    csv_to_xlsx_p.add_argument("--output", dest="output", required=True, help="Выходной XLSX файл")
+
+    args = parser.parse_args()#Парсим аргументы командной строки и сохраняем в объект args
 
     if args.cmd == "json2csv":
-        #python -m src.lab06.cli_convert json2csv --in data/samples/people.json --out data/out/people_from_json.csv
+        # py -m src.lab06.cli_convert json2csv --input  data/samples/ex1.json  --output data/out/ex1_from_json.csv
         json_to_csv(json_path=args.input, csv_path=args.output)
-
     elif  args.cmd == "csv2json":
-        #python -m src.lab06.cli_convert csv2json --in data/samples/people.csv --out data/out/people_from_csv.json
+        #py -m src.lab06.cli_convert csv2json --input data/samples/ex2.csv --output data/out/ex2_from_csv.json
         csv_to_json(csv_path=args.input, json_path=args.output)
-
     elif args.cmd == "csv2xlsx":
-        #python -m src.lab06.cli_convert csv2xlsx --in data/samples/people.csv --out data/out/people.xlsx
+        #py -m src.lab06.cli_convert csv2xlsx --input data/samples/ex2.csv --output data/out/ex2_from_csv.xlsx
         csv_to_xlsx(csv_path=args.input, xlsx_path=args.output)
 
 if __name__ == "__main__":
